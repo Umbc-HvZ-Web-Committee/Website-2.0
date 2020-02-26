ca859613446c1baf479a7bd9ee9f35441299be94
Kyle J Mosier <kyle@mosier.org>
2020-02-26 01:42:55 -0500

    Probably fixed the sidebar page so it doesn't error out

--- admin/editSidebar.php
+++ admin/editSidebar.php
@@ -1,11 +1,12 @@
 <?php
-require_once('pageIncludes/editSidebar.inc.php');
+require_once('pageIncludes/myProfile.inc.php');
+require_once('includes/update.php');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head><?php placeTabIcon(); ?>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>UMBC HvZ</title>
+<title>UMBC HvZ - Admin Panel</title>
 <meta name="keywords" content="" />
 <meta name="description" content="" />
 <link href="/style.css" rel="stylesheet" type="text/css" media="all" />
@@ -13,18 +14,16 @@ require_once('pageIncludes/editSidebar.inc.php');
 <?php htmlHeader(); ?>
 </head>
 <body>
-<a name="top"></a>
 <div id="wrapper">
 	<?php pageHeader(); ?>
 	<div id="page" class="container">
 		<div id="content">
-
-		<?php if($_SESSION['isAdmin'] >= 2){ ?>
+			<?php
+			<?php if($_SESSION['isAdmin'] >= 2){ ?>
 			<h2>Yeet</h2>
 		<?}else{ ?>
-			<h2>Hey, you're not an admin, get out of here!</h2>
+			<h2>Big oof</h2>
 		<? } ?>
-
 		</div>
 		<?php printSidebar(); 
 		?>

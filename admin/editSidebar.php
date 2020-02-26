33e3c97ddc68678fc09e693a4bb4817a32c6a92a
Kyle J Mosier <kyle@mosier.org>
2020-02-26 15:30:33 -0500

    Added warning not to mess with the sidebar editor, yet

--- admin/editSidebar.php
+++ admin/editSidebar.php
@@ -18,6 +18,8 @@ require_once('../pageIncludes/admin/editSidebar.inc.php');
 	<div id="page" class="container">
 		<div id="content">
 			<?php if($_SESSION['isAdmin'] >= 2){ ?>
+			
+				<b>WARNING: Nothing on thispage has any functionaliy yet. Please do not interact with this unless you have been directed to do so by the web committee or it actually works properly now and I just forgot to clean this warning up</b><br/>
 				<h2>Sidebar settings</h2>
 				<form action="" method="post">
 				<label for="baseSlides"><input type="radio" name="setSlides" value="no"/>Display one-night slides on the sidebar</label></br>

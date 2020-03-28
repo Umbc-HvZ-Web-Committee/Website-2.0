7f5553361c5953b112e4e7aad9df9587d7341872
mosier13 <mosier1@umbc.edu>
2020-03-28 01:17:17 -0400

    Testing a bugfix that may fix newline characters in the OZ selection page

--- admin/ozSelect.php
+++ admin/ozSelect.php
@@ -40,7 +40,7 @@ $playerData = mysql_oneline("SELECT * FROM users WHERE UID='{$_SESSION['uid']}'"
 					."ORDER BY long_players.playerID";
 					$qury = mysql_query($sql);
 					while($ret = mysql_fetch_assoc($qury)){
-						$para = $ret['ozParagraph'];
+						$para = preg_replace("/\\\\*'/","'",$ret['ozParagraph']);
 						//$para = preg_replace("/\\\\+('|\")/","\\1",$para);
 						echo "<tr>";
 						if($ret['state'] == 2) {

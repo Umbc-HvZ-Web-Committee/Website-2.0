b6933e94ed04cdb2282caba52d3e2559c381f394
mosier13 <mosier1@umbc.edu>
2020-03-31 13:20:05 -0400

    Fixed SQL queries for monday/thursday slides and cleaned up debug code

--- pageIncludes/admin/editSidebar.inc.php
+++ pageIncludes/admin/editSidebar.inc.php
@@ -198,12 +198,10 @@ if($_SESSION['isAdmin'] >= 2) {
 				mysql_query("UPDATE mission_slide_headings SET headingName = '$mainHeading' WHERE headingTitle = 'mainHeading';");
 			}
 			if($firstSlides != NULL) {
-				echo $firstSlides;
-				mysql_query("UPDATE mission_slide_headings SET headingName = '$firstSlides'; WHERE headingTitle = 'firstSlides'");
+				mysql_query("UPDATE mission_slide_headings SET headingName = '$firstSlides'; WHERE headingTitle = 'firstSlides';");
 			}
 			if($secondSlides != NULL) {
-				echo $secondSlides;
-				mysql_query("UPDATE mission_slide_headings SET headingName = '$secondSlides'; WHERE headingTitle = 'secondSlides'");
+				mysql_query("UPDATE mission_slide_headings SET headingName = '$secondSlides'; WHERE headingTitle = 'secondSlides';");
 			}
 			if($thirdSlides == "IGNORE") {
 				mysql_query("UPDATE mission_slide_headings SET headingName = NULL WHERE headingTitle = 'thirdSlides';");

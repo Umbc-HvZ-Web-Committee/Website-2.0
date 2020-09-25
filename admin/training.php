0359b8f9c0355747c4fba8bb3cc33c7e1699e21b
mosier13 <mosier1@umbc.edu>
2020-09-25 14:22:57 -0400

    Ok, I give up on video embeds. You get direct links to videos instead

--- admin/training.php
+++ admin/training.php
@@ -27,27 +27,31 @@ $settings = get_settings();
 			for content that does not exist yet. All new officers & web committee members should be directed
 			to this page so these resources can serve as website training for them.</p>
 			
-			<h3>Basic <strike>Normie</strike> Website Walkthrough</h3>
-			<embed src="https://www.youtube.com/embed/watch?v=TSN0s06Ip_8" width="480" height="286" scale="aspect" controller="true">
+			<a href="https://www.youtube.com/watch?v=TSN0s06Ip_8"><h3>Basic <strike>Normie</strike> Website Walkthrough</h3></a>
+			<p>An introduction to the Humans vs Zombies Website, 
+			looking through the pages that are available to general users who have an account</p>
 			
-			<h3>Admin Panel Walkthrough</h3>
-			<embed src="https://www.youtube.com/embed/watch?v=B6UVxGpAp48" width="480" height="286" scale="aspect" controller="true">
+			<a href="https://www.youtube.com/watch?v=tUPAEv9ZMSU"><h3>Admin Panel Walkthrough</h3></a>
+			<p>An introduction to the Admin Panel, a series of pages available exclusively to the officers and the web committee for 
+			manipulating the settings of the website and viewing restricted information</p>
+			
 			
 			<!-- The yt links are shitpost placeholders for future content to be put here -->
 			
-			<h3>Weeklong Logistics Walkthrough</h3>
-			<embed src="https://www.youtube.com/watch?v=6Dh-RL__uN4" width="1440" height="858" scale="aspect" controller="true">
+			<a href="https://www.youtube.com/watch?v=6Dh-RL__uN4"><h3>Weeklong Logistics Walkthrough</h3></a>
+			<p>A look through the various website pages that relate to weeklongs and a guide for weeklong moderators in using them</p>
 			
-			<h3>Election Logistics Walkthrough</h3>
-			<embed src="https://www.youtube.com/watch?v=U06jlgpMtQs" width="1440" height="858" scale="aspect" controller="true">
+			<a href="https://www.youtube.com/watch?v=U06jlgpMtQs"><h3>Election Logistics Walkthrough</h3></a>
+			<p>A guide on how the website can be used to administer the club's elections</p>
+			
 		
 		<?php }else if($_SESSION['isAdmin'] >= 3){ ?>
 			<h2>Additional Resources (Web Committee Only)</h2>
 
-			<h3>Web Committee GitHub and Database Access</h3>
-			<embed src="https://www.youtube.com/watch?v=Qskm9MTz2V4" width="1440" height="858" scale="aspect" controller="true">
-			
-			
+			<a href="https://www.youtube.com/watch?v=Qskm9MTz2V4"><h3>Web Committee GitHub and Database Access</h3></a>
+			<p>A tutorial for new web committee members on how to access and modify the GitHub repository for the website's source code 
+			and on how to access and modify the club's database through PHPMyAdmin</p>
+
 			
 		<?php }else{ ?>
 			<h2>Hey, you're not an admin, get out of here!</h2>

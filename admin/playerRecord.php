c45463cf356b45d84c2406805a49aa0465983bb4
mosier13 <mosier1@umbc.edu>
2022-09-12 15:08:01 -0400

    Update player record page to use the correct path for pulling its pageIncludes file

--- admin/playerRecord.php
+++ admin/playerRecord.php
@@ -1,5 +1,5 @@
 <?php
-require_once('../pageIncludes/admin/vaccine.inc.php');
+require_once('../pageIncludes/admin/playerRecord.inc.php');
 $settings = get_settings();
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">

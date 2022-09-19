7f7a6b5dd9cd5dcc7bb4f09ffab87d1174c2eb6a
mosier13 <mosier1@umbc.edu>
2022-09-19 13:44:23 -0400

    Fixed calls to deumerate() to include both required args

--- myProfile.php
+++ myProfile.php
@@ -219,13 +219,13 @@ require_once('includes/update.php');
 					
 					echo "</br><h3>Waiver Status</h3></br>";
 					$waiverStatus = $ret['hasTurnedInWaiver'];
-					$waiverStatus = denumerate($waiverStatus);
+					$waiverStatus = denumerate('waiverStatus', $waiverStatus);
 					echo "Your waiver status is $waiverStatus <br/>";
 					echo "By UMBC rules, all players are required to have a waiver turned in each year to participate in UMBC HvZ games. If your waiver status is listed above as \"Waivered\", then our records indicate that you have filled out a waiver. Our records will not update right away, so it is posible that you have filled out a waiver, but your status above indicates otherwise. If you have <strong>not</strong> filled out a waiver, you can fill out an online waiver <a href=\"https://umbcorgs.dserec.com/online/clubsports_widget/club/84/registration\">here</a>. If you are unable to fill out an online waiver, you can request a paper waiver from an officer at one of our meetings.";					
 					
 					echo "</br><h3>Vaccination Status</h3></br>";
 					$vaccineStatus = $ret['vaccineStatus'];
-					$vaccineStatus = denumerate($vaccineStatus);
+					$vaccineStatus = denumerate('vaccineStatus', $vaccineStatus);
 					echo "Your coronavirus vaccination status is $vaccineStatus <br/>";
 					echo "NOTE: By UMBC rules, players are <i>no longer required</i> to be vaccinated to participate in UMBC HvZ games as of Fall 2022. Most of you that are vaccinated will be listed as unvaccinated above since our website's vaccination records are not up-to-date.";
 					

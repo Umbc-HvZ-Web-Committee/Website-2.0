fc6e7be378e16158dda02d8056c3f00d4720fb88
mosier13 <mosier1@umbc.edu>
2022-09-27 17:49:55 -0400

    Reformatted how headers are printed

--- admin/awardAchieves.php
+++ admin/awardAchieves.php
@@ -20,7 +20,8 @@ $playerData = mysql_oneline("SELECT * FROM users WHERE UID='{$_SESSION['uid']}'"
 		<div id="content">
 		<?php if($_SESSION['isAdmin'] >= 3){ ?>
 
-			<h1 style="text-align:center">Award an Achievement to an individual player</h1><br/>
+			<h1 style="text-align:center">Award an Achievement</h1><br/><br/>
+			<br/><h2 style="text-align:center">Achievements for an individual player</h1><br/>
 			<form action="" method="post">
 			Username or Email: <input type="text" name="uname"></input><br/>
 			Achievement: 
@@ -32,7 +33,7 @@ $playerData = mysql_oneline("SELECT * FROM users WHERE UID='{$_SESSION['uid']}'"
 			<input type="submit" name="submit" value="Individual Achievement"></input>
 			</form>
 			
-			<h1 style="text-align:center">Award an Achievement to a group (Web Committee only)</h1><br/>
+			<br/><h2 style="text-align:center">Achievements for a group of players (Web Committee Only)</h1><br/>
 			<p>Write the WHERE clause of a SQL query that defines a group which you want to give the same achievement to in the "Group definition" box. Test this SQL query in PHPMyAdmin or other SQL enviornment before running here to ensure that it is correct. Since this involves directly writing a major portion of a SQL query, out of an adundance of caution, all input strings containing the SQL query terminal token (semicolon) and/or the SQL query comment token (double dash) will be rejected.</p>
 			<p>Example test query: <strong>SELECT `uid`, `uname`, `fname`, `lname` FROM `users` WHERE (YOUR INPUT GOES HERE WITHOUT PARANTHESES);</strong></p>
 			<form action="" method="post">

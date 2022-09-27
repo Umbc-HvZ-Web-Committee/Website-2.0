9381cf4e3976d2c005d047c777e717e401e5ee68
mosier13 <mosier1@umbc.edu>
2022-09-27 17:19:19 -0400

    Added support for awarding achievements to a group of users by providing the WHERE clause of an SQL query (only to be used by web committee), pending testing

--- admin/awardAchieves.php
+++ admin/awardAchieves.php
@@ -18,9 +18,9 @@ $playerData = mysql_oneline("SELECT * FROM users WHERE UID='{$_SESSION['uid']}'"
 	<?php pageHeader(); ?>
 	<div id="page" class="container">
 		<div id="content">
-		<?php if($_SESSION['isAdmin'] >= 1){ ?>
+		<?php if($_SESSION['isAdmin'] >= 3){ ?>
 
-			<h1 style="text-align:center">Award an Achievement</h1><br/>
+			<h1 style="text-align:center">Award an Achievement to an individual player</h1><br/>
 			<form action="" method="post">
 			Username or Email: <input type="text" name="uname"></input><br/>
 			Achievement: 
@@ -29,7 +29,38 @@ $playerData = mysql_oneline("SELECT * FROM users WHERE UID='{$_SESSION['uid']}'"
 					generateList();
 					?>
 				</select><br/>
-			<input type="submit" name="submit" value="Submit"></input>
+			<input type="submit" name="submit" value="Individual Achievement"></input>
+			</form>
+			
+			<h1 style="text-align:center">Award an Achievement to a group (Web Committee only)</h1><br/>
+			<p>Write the WHERE clause of a SQL query that defines a group which you want to give the same achievement to in the "Group definition" box. Test this SQL query in PHPMyAdmin or other SQL enviornment before running here to ensure that it is correct. Since this involves directly writing a major portion of a SQL query, out of an adundance of caution, all input strings containing the SQL query terminal token (semicolon) and/or the SQL query comment token (double dash) will be rejected.</p>
+			<p>Example test query: <strong>SELECT `uid`, `uname`, `fname`, `lname` FROM `users` WHERE (YOUR INPUT GOES HERE WITHOUT PARANTHESES);</strong></p>
+			<form action="" method="post">
+			Group definition: <input type="text" name="whereClause"></input><br/>
+			Achievement: 
+				<select name="achieve">
+					<?php 
+					generateList();
+					?>
+				</select><br/>
+			<input type="submit" name="submit" value="Group Achievement"></input>
+			</form>
+			
+			<?php 
+			echo $status;
+				
+		} else if($_SESSION['isAdmin'] >= 1){ ?>
+
+			<h1 style="text-align:center">Award an Achievement to an individual player</h1><br/>
+			<form action="" method="post">
+			Username or Email: <input type="text" name="uname"></input><br/>
+			Achievement: 
+				<select name="achieve">
+					<?php 
+					generateList();
+					?>
+				</select><br/>
+			<input type="submit" name="submit" value="Individual Achievement"></input>
 			</form>
 			
 			<?php 
--- pageIncludes/admin/awardAchieves.inc.php
+++ pageIncludes/admin/awardAchieves.inc.php
@@ -11,59 +11,96 @@ if(isset($_REQUEST['submit'])){
 	
 	$func = $_REQUEST['submit'];
 	$value = $_REQUEST['achieve'];
-	$playerID = requestVar('uname');
-	$ret = getUID($playerID);
-	if(!$ret) {
-		$status = "</br><h3>Player not found.</h3>";
-	} else {
-			
-		$uid = $ret['UID'];
-		$email = $ret['email'];
-		$name = $ret['fname']." ".$ret['lname'];
-		
-		$achieveLookupSql = "SELECT * FROM `achievements_new` WHERE `key`='$value'";
-		$achieveLookupRet = mysql_oneline($achieveLookupSql);
-		$achieveAID = $achieveLookupRet['AID'];
-		$achieveName = $achieveLookupRet['name'];
-		
-		if(!giveAchieve($achieveAID, $uid))
-		{
-			$status = "</br><h3>Player already has that achievement.</h3>";
+	//echo($func);
+	if($func=="Individual Achievement"){
+		$playerID = requestVar('uname');
+		$ret = getUID($playerID);
+		if(!$ret) {
+			$status = "</br><h3>Player not found.</h3>";
 		} else {
-			$status = "</br><h3>".$name." has been awarded the achievement ".$achieveName.".</h3>";
+				
+			$uid = $ret['UID'];
+			$email = $ret['email'];
+			$name = $ret['fname']." ".$ret['lname'];
+			
+			$achieveLookupSql = "SELECT * FROM `achievements_new` WHERE `key`='$value'";
+			$achieveLookupRet = mysql_oneline($achieveLookupSql);
+			$achieveAID = $achieveLookupRet['AID'];
+			$achieveName = $achieveLookupRet['name'];
+			
+			if(!giveAchieve($achieveAID, $uid))
+			{
+				$status = "</br><h3>Player already has that achievement.</h3>";
+			} else {
+				$status = "</br><h3>".$name." has been awarded the achievement ".$achieveName.".</h3>";
+			}
+			
+			
+			$sql = "SELECT * FROM `userAchieveLink_new` WHERE UID='$uid'";
+			$retTwo = mysql_query($sql);
+			while($row = mysql_fetch_assoc($retTwo)) {
+				if($achieveAID == $row['AID']) {
+					$found = true;
+					break;
+				}
+			}
+		}
+	}else if($func=="Group Achievement"){
+		$whereClause = requestVar('whereClause');
+		
+		//Check for safe query before proceeding
+		
+		//The condition below is equivalent to checking if either substring is in $whereClause as explained below:
+		//the strpos function returns the index value of the first occurence of the second paramater in the first paramater,
+		//and two characters cannot occupy the same index of a string, so for strpos() to return the same value for both,
+		//that would mean neither substring is in the string $whereClause (which is what we want), 
+		//which would return a value of false for both, and since false == false, that would indicate that neither
+		//SQL token of interest is in the provided input, making this query safe for the level of scrutiny we provide here.
+		//Thus, we may proceed.
+		if(strpos($whereClause, ";") != strpos($whereClause, "--")) {
+			//Bad query
+			status = "Illegal query provided!";
+			break;
 		}
 		
+		$unames = mysql_query("SELECT `uname` FROM `users` WHERE $whereClause;");
+		$status = "";
 		
-		$sql = "SELECT * FROM `userAchieveLink_new` WHERE UID='$uid'";
-		$retTwo = mysql_query($sql);
-		while($row = mysql_fetch_assoc($retTwo)) {
-			if($achieveAID == $row['AID']) {
-				$found = true;
-				break;
+		foreach($unames as $playerID) {
+			$ret = getUID($playerID);
+			if(!$ret) {
+				$status = $status."</br><h3>Player ".$playerID." not found.</h3>";
+			} else {
+					
+				$uid = $ret['UID'];
+				$email = $ret['email'];
+				$name = $ret['fname']." ".$ret['lname'];
+				
+				$achieveLookupSql = "SELECT * FROM `achievements_new` WHERE `key`='$value'";
+				$achieveLookupRet = mysql_oneline($achieveLookupSql);
+				$achieveAID = $achieveLookupRet['AID'];
+				$achieveName = $achieveLookupRet['name'];
+				
+				if(!giveAchieve($achieveAID, $uid))
+				{
+					$status = $status."</br><h3>Player already has that achievement.</h3>";
+				} else {
+					$status = $status."</br><h3>".$name." has been awarded the achievement ".$achieveName.".</h3>";
+				}
+				
+				
+				$sql = "SELECT * FROM `userAchieveLink_new` WHERE UID='$uid'";
+				$retTwo = mysql_query($sql);
+				while($row = mysql_fetch_assoc($retTwo)) {
+					if($achieveAID == $row['AID']) {
+						$found = true;
+						break;
+					}
+				}
 			}
 		}
 		
 		
-		/*if($found) {//Displays incorrect case
-			$status = "</br><h3>Player already has that achievement.</h3>";
-		} else {		
-			mysql_query("INSERT INTO `userAchieveLink_new`(`AID`, `UID`) VALUES ('$achieveAID','$uid');");
-			$status = "</br><h3>".$name." has been awarded the achievement ".$achieveName.".</h3>";
-			
-			$subject = "Achievement \"$achieveName\" Awarded";
-			$msg = <<<EOF
-Congratulations!
-			
-You have been awarded the achievement $achieveName! To set this achievement as your displayed achievement on 'umbchvz.com/playerList', go to 'umbchvz.com/myProfile' and log in. Under the 'Achievements' header, select this achievement from the dropdown menu and click 'Submit'. This achievement should now be displayed in the same row as your name on the Player List.
-			
-Please note that artwork has not been chosen for all achievements and the image may still say 'Coming Soon'. If this is the case, hover your mouse over the image to verify the proper achievement was selected. The artwork will automatically update as it becomes available.
-			
-~ Kyle J Mosier - Webmaster ~
-THIS IS AN AUTOMATED MESSAGE. 
-EOF;
-			
-			mail($email, $subject, $msg);
-		}*/
 	}
 } else {
 	$status = "";

846e3e1dad1e3472fad0d219231859c7ad60fdab
SethRamsland <124908308+SethRamsland@users.noreply.github.com>
2023-09-07 18:39:37 -0400

    Update TOS.php

--- TOS.php
+++ TOS.php
@@ -4,6 +4,7 @@ require_once('includes/quick_con.php');
 load_config('config.txt');
 my_quick_con($config);
 $ret = mysql_oneline("SELECT `value` FROM `settings` WHERE `key`='TOS';");
+echo "test";
 echo $ret['value'];
 ?>
 </body></html>

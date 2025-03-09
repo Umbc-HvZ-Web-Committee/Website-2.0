09044ff1889281343b730ab9fc42258584249eb0
Fernando Chicas <114684683+linktheking12@users.noreply.github.com>
2025-03-08 22:29:13 -0500

    the bruhenning

--- voting.php
+++ voting.php
@@ -176,7 +176,7 @@ $settings = get_settings();
 					
 					if($settings['lockVoting'] == "unlock") {
 						echo '<input type="checkbox" id="studentCheck" name="test">'
-						document.getElementById("studentCheck").required = true;
+						//document.getElementById("studentCheck").required = true;
 						echo '<input type="submit" name="submit" value="Submit vote"></form>';
 					} else {
 						echo "<h2 style=\"text-align:center\">Voting is currently closed.</h2>";

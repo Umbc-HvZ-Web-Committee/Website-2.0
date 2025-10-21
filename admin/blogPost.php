7cdea4103a051161eaa3b523c3cb30e012f4db78
Eragonnogare <43831501+Eragonnogare@users.noreply.github.com>
2025-10-21 18:50:37 -0400

    Maybe it's the small things that count
    Maybe...

--- admin/blogPost.php
+++ admin/blogPost.php
@@ -42,7 +42,7 @@ $playerData = mysql_oneline("SELECT * FROM users WHERE UID='{$_SESSION['uid']}'"
 					var content = marked.parse(element.value);
 					element.value = content;
 				}
-				document.querySelector(input[type="submit"]).addEventListener("click", fixMarkup);
+				document.querySelector(input[type="submit"]).addEventListener("submit", fixMarkup);
 				window.fixMarkup = fixMarkup;
 			</script>
 		

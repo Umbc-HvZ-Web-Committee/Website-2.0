99d4a38d4e1a58536cf62ac2f13412591bd1f557
Eragonnogare <43831501+Eragonnogare@users.noreply.github.com>
2025-10-29 04:51:27 -0400

    Was overzealous, *now* deleting should probably work.

--- news.php
+++ news.php
@@ -34,7 +34,7 @@ require_once('pageIncludes/home.inc.php');
 	var confirmButtons = document.querySelectorAll("confirm");
 
 	for(let i = 0; i < deleteButtons.length; i++) {
-  		deleteButtons[i].id.onclick = function() {
+  		deleteButtons[i].onclick = function() {
     		confirmButtons[i].id.style.display = "inline-block";
 		}
 

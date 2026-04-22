76112f4958eda715c52a825afd851eb149268206
Eragonnogare <43831501+Eragonnogare@users.noreply.github.com>
2026-04-21 21:43:38 -0400

    Testing

--- pageIncludes/admin/editVoting.inc.php
+++ pageIncludes/admin/editVoting.inc.php
@@ -30,7 +30,7 @@ if($_SESSION['isAdmin'] >= 2) {
 					mysql_query("INSERT INTO `election_votes` (`uid`, `position`, `voteFor`) VALUES ('$nullUID', '$position', '$name');");
 					$GLOBALS['submitMessage'] = "Added candidate with a blank bio";
 				} else {
-					//mysql_query("INSERT INTO `election_candidates` (`position`, `name`, `bio`) VALUES ('$position', '$name', '$bio');");
+					mysql_query("INSERT INTO `election_candidates` (`position`, `name`, `bio`) VALUES ('$position', '$name', '$bio');");
 					mysql_query("INSERT INTO `election_votes` (`uid`, `position`, `voteFor`) VALUES ('$nullUID', '$position', '$name');");
 					$GLOBALS['submitMessage'] = "Added candidate and bio";
 				}
